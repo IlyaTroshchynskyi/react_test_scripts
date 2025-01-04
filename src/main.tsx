5ed82accdbf4964a1b0856776884a5f2e6dd5a91
Illia Troshchynskyi <illia.troshchynskyi@nixs.com>
2025-01-04 17:14:35 +0100

    some

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
-import * as ReactDOM from "react-dom/client";
-import * as React from "react";
+import * as ReactDOM from 'react-dom/client'
+import * as React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 )
